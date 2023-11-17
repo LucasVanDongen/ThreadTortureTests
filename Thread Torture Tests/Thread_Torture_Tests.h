73ced2588b59d048351826a48aa7b5e628cebf6d
Lucas van Dongen <lucas.van.dongen@gmail.com>
2023-11-17 09:27:44 +0100

    Initial Commit

---
+++ Thread Torture Tests/Thread_Torture_Tests.h
@@ -0,0 +1,18 @@
+//
+//  Thread_Torture_Tests.h
+//  Thread Torture Tests
+//
+//  Created by Lucas van Dongen on 17/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Thread_Torture_Tests.
+FOUNDATION_EXPORT double Thread_Torture_TestsVersionNumber;
+
+//! Project version string for Thread_Torture_Tests.
+FOUNDATION_EXPORT const unsigned char Thread_Torture_TestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Thread_Torture_Tests/PublicHeader.h>
+
+
